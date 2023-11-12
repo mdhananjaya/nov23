bc69c68a7c64fa8bb452a8d4b5dc9ab1d11eec6e
mdhananjaya <m_dhana2003@yahoo.com>
2023-11-11 20:18:53 -0800

    version2

--- domain.py
+++ domain.py
@@ -12,4 +12,5 @@ cmo.setListenAddress('192.168.0.153')
 writeDomain('/u01/app/oracle/sept23/'+ location +'/user_projects/domains/'+ location1)
 closeTemplate()
 print '>>>Domain created successfully>>>'
+print 'Use the Domain now'
 exit()
